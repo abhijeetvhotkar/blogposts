92cd3a7654f9730e0709873908883e1ad045d45d
abhijeet <avhotkar@careclix.com>
2020-07-30 00:03:46 -0400

    Build moderation service and start adding docker files

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,17 @@ import React from "react";
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has be rejected";
+    }
+    if (comment.status === "pending") {
+      content = "This comment is pending moderation";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
